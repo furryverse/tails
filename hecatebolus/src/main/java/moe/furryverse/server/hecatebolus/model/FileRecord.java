b8593299b6271f8f09c2a580c2e67b352e58de5e
hanbings <hanbings@hanbings.io>
2023-08-26 20:13:14 +0800

    📝 Docs: Update readme.

--- hecatebolus/src/main/java/moe/furryverse/server/hecatebolus/model/FileRecord.java
+++ hecatebolus/src/main/java/moe/furryverse/server/hecatebolus/model/FileRecord.java
@@ -8,14 +8,14 @@ import org.springframework.data.mongodb.core.mapping.Field;
 /**
  * 文件记录
  *
- * @param fid      文件 ID
- * @param created  创建时间
- * @param uid      用户账户 ID
- * @param name     文件名
- * @param path     文件路径
- * @param type     文件类型
- * @param size     文件大小
- * @param isPublic 是否公开
+ * @param fileId    文件 ID
+ * @param created   创建时间
+ * @param accountId 用户账户 ID
+ * @param name      文件名
+ * @param path      文件路径
+ * @param type      文件类型
+ * @param size      文件大小
+ * @param isPublic  是否公开
  */
 @Document("file_record")
 public record FileRecord(
