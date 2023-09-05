a05b34432408a70904d7ec54a00a57343134df04
hanbings <hanbings@hanbings.io>
2023-09-05 20:12:54 +0800

    🐛 Fix: fix build.

--- hecatebolus/src/main/java/moe/furryverse/server/hecatebolus/controller/FileController.java
+++ hecatebolus/src/main/java/moe/furryverse/server/hecatebolus/controller/FileController.java
@@ -1,20 +1,16 @@
 package moe.furryverse.server.hecatebolus.controller;
 
-import moe.furryverse.server.common.annotation.AccessCheck;
-import moe.furryverse.server.common.security.Access;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v0")
 public class FileController {
     @PutMapping("/upload")
-    @AccessCheck(access = {Access.FILE_UPLOAD})
     public void uploadFile() {
 
     }
 
     @GetMapping("/file")
-    @AccessCheck(access = {Access.FILE_LIST})
     public void listFiles() {
 
     }
@@ -25,7 +21,6 @@ public class FileController {
     }
 
     @DeleteMapping("/file/:fileId")
-    @AccessCheck(access = {Access.FILE_DELETE})
     public void deleteFile() {
 
     }
